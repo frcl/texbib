c005eb9759dddf0e75ddd8684ff4340498cce552
Lars Franke <lars.ch.franke@gmail.com>
2016-02-07 17:42:42 +0100

    Moveing commands from main file into package as commands.py

--- texbib.py
+++ texbib.py
@@ -1,107 +1,53 @@
 #!/bin/env python3.5
 # -*- coding: utf-8 -*-
+"""
+This is the main file of the texbib program. A program that helps you
+to manage your BibTeX references.
+"""
 
-import os
-import argparse
+import os as os
+import argparse as argparse
 
-from texbib.bibliography import *
+from texbib.bibliography import BibNameError, DatabaseError
+from texbib.commands import CmdParser
 
-def _main(args):
+def tell(msg):
+    print('texbib: {}'.format(msg))
+
+def fail(msg):
+    print('texbib: {}'.format(msg))
+    quit()
+
+def main(args):
 
     cmd = args.command
     cmd_args = args.args
-    cmd_parser = _CmdParser()
+    cmd_parser = CmdParser(fail, tell)
 
-    if hasattr(cmd_parser, cmd):
+    if hasattr(cmd_parser, cmd) and not cmd.startswith('__'):
         cmd_func = getattr(cmd_parser, cmd)
         try:
             cmd_func(*cmd_args)
         except TypeError:
-            _fail('wrong number of arguments')
+            fail('wrong number of arguments')
         except BibNameError:
-            _fail('unknown bibname')
+            fail('unknown bibname')
         except DatabaseError:
-            _fail('database currupt')
+            fail('database currupt')
     else:
-        _fail('unknown command')
-
-def _tell(msg):
-    print('texbib: {}'.format(msg))
-
-def _fail(msg):
-    print('texbib: {}'.format(msg))
-    quit()
-
-class _CmdParser(object):
-    def add(self, *filenames):
-        self.addto('all', *filenames)
-
-    def rm(self, *identifyer):
-        self.rmfrom('all', *identifyer)
-
-    def dump(self, bibname='all', directory='.'):
-        if not os.path.exists(directory):
-            _fail("'{}' is not a directory".format(directory))
-        with Bibliography(bibname) as bib:
-            path = os.path.join(
-                    directory,'{}.bib'.format(bibname))
-            with open(path,'w') as f:
-                f.write(bib.bibtex())
-
-    def show(self, bibname):
-        with Bibliography(bibname) as bib:
-            print(bib)
-
-    def mkbib(self, bibname):
-        with Bibliography(bibname,mode='m') as bib:
-            pass
-
-    def rmbib(self, bibname):
-        with Bibliography(bibname) as bib:
-            os.remove(bib.path)
-
-    def addto(self, bibname, *filenames):
-        with Bibliography(bibname) as bib:
-            for fn in list(filenames):
-                if os.path.exists(fn):
-                    try:
-                        with open(fn,'r') as f:
-                            bib.update(f.read())
-                    except BibCodeError:
-                        _tell(
-                        "invalid Bibtex in file '{}'".format(fn))
-#                    except Exception:
-#                        _tell(
-#                        "can not open file '{}'".format(fn))
-                else:
-                    _tell(
-                    "file '{}' not in directory".format(fn))
-
-    def rmfrom(self, bibname, *identifyer):
-        with Bibliography(bibname) as bib:
-            for Id in list(identifyer):
-                try:
-                    del bib[Id]
-                except BibKeyError:
-                    _fail(
-                    "No item with ID '{}' in {}".format(
-                            Id,bibname))
-
-    def cleanup(self, bibname='all'):
-        with Bibliography(bibname) as bib:
-            bib.cleanup()
-
-    def chid(self, identifyer, new_identifyer):
-        pass
-
-    def chcont(self, identifyer, attribute, value):
-        pass
+        fail('unknown command')
 
 if __name__ == '__main__':
-    argp = argparse.ArgumentParser()
+    argp = argparse.ArgumentParser(
+            description='Texbib is a program that helps '
+            'you to manage your BibTeX references.')
 
-    argp.add_argument('command')
-    argp.add_argument('args', nargs='*')
+    argp.add_argument('command', help='Texbib command to be executed')
+    #choices=('add','addto','show','searchin','rm','rmfrom','mkbib',
+    #'rmbib'))
+    argp.add_argument('args', nargs='*',
+            help='arguments for Texbib command, '
+            'number and meaning depends on command')
 
-    _main(argp.parse_args())
+    main(argp.parse_args())
 
---
+++ texbib/commands.py
@@ -0,0 +1,76 @@
+from .bibliography import *
+
+class CmdParser:
+
+    def __init__(self, failfunc=lambda x: None, tellfunc=lambda x: None):
+        self.fail = failfunc
+        self.tell = tellfunc
+
+    def add(self, *filenames):
+        self.addto('all', *filenames)
+
+    def rm(self, *identifyer):
+        self.rmfrom('all', *identifyer)
+
+    def dump(self, bibname='all', directory='.'):
+        if not os.path.exists(directory):
+            self.fail("'{}' is not a directory".format(directory))
+        with Bibliography(bibname) as bib:
+            path = os.path.join(
+                    directory, '{}.bib'.format(bibname))
+            with open(path,'w') as f:
+                f.write(bib.bibtex())
+
+    def show(self, bibname):
+        with Bibliography(bibname) as bib:
+            print(bib)
+
+    def mkbib(self, bibname):
+        with Bibliography(bibname, mode='m') as bib:
+            pass
+
+    def rmbib(self, bibname):
+        with Bibliography(bibname) as bib:
+            os.remove(bib.path)
+
+    def addto(self, bibname, *filenames):
+        with Bibliography(bibname) as bib:
+            for fn in list(filenames):
+                if os.path.exists(fn):
+                    try:
+                        with open(fn, 'r') as f:
+                            bib.update(f.read())
+                    except BibCodeError:
+                        self.tell(
+                        "invalid Bibtex in file '{}'".format(fn))
+                    except IOError:
+                        self.tell(
+                        "can not open file '{}'".format(fn))
+                else:
+                    self.tell(
+                    "file '{}' not in directory".format(fn))
+
+    def rmfrom(self, bibname, *identifyer):
+        with Bibliography(bibname) as bib:
+            for Id in list(identifyer):
+                try:
+                    del bib[Id]
+                except BibKeyError:
+                    self.fail(
+                    "No item with ID '{}' in {}".format(
+                            Id, bibname))
+
+    def searchin(self, bibname, pattern):
+        with Bibliography(bibname) as bib:
+            print('\n'.join(map(str, bib.search(pattern))))
+
+    def cleanup(self, bibname='all'):
+        with Bibliography(bibname) as bib:
+            bib.cleanup()
+
+    def chid(self, identifyer, new_identifyer):
+        pass
+
+    def chcont(self, identifyer, attribute, value):
+        pass
+
